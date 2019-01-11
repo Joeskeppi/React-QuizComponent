d6bb21245261501851091b2b0baf91aad90777a1
Joe Howells <joe.howells@axa.co.uk>
2019-01-11 15:41:59 +0000

    typo on class name

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
     render() {
-        return(<div><div ClassName='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div></div>);
+        return(<div><div className='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div></div>);
     }
 }
 
