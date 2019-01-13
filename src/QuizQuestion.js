b25d454fae8789dd77c3240cbdc1ad1987934f2e
Joe Howells <joeskeppi@gmail.com>
2019-01-13 14:51:39 +0000

    chapter 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -24,7 +24,7 @@ class QuizQuestion extends Component {
   }
 
   handleClick (buttonText) {
-      if (buttonText === this.props.answer_option)
+      if (buttonText === this.props.quiz_question.answer)
       {
         this.props.showNextQuestionHandler()
       }
